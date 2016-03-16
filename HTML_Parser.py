39820ddaf49b84b4f2caa1553db1f9afc51cb0c1
Prasanna Sajjan <prasanna.sajjan@gmail.com>
2016-03-15 23:07:37 -0400

    HTML Parser
    A simple HTML Parser which be used to scan the html files and extract
    only words from them and ignore all the tags and other styling elements
    present inside the HTML file.
    
    This program will accept only one file at a time and output the words
    into a text file. This text file in turn can be used as an input to
    stemmers such as Porter or Krovetz. (That was my original intention to
    write this program)

---
+++ HTML_Parser.py
@@ -0,0 +1,29 @@
+import urllib
+import itertools
+import codecs
+import sys
+from bs4
+import BeautifulSoup
+url = r "/root/data/en/articles/your_input_file.html"
+page = open(url)
+soup = BeautifulSoup(page.read())
+for script in soup(["script", "style"]):
+    script.extract()
+text = soup.get_text()
+lines = (line.strip() for line in text.splitlines())
+data_lines = []
+for i in lines:
+    if len(i) != 0:
+    3
+data_lines.append(i)
+actual = []
+for i in data_lines:
+    if len(i) != 1:
+    actual.append(i)
+words = []
+for line in actual:
+    words.append(line.strip().split(’’))
+merged = list(itertools.chain( * words))
+with codecs.open("/root/sample.txt", "w", encoding = "utf-8") as f:
+    for i in merged:
+    f.write(i + ’\n’)
